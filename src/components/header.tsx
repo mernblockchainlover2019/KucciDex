4103b22e2fff938e442dfae0965e2194b5312eac
Andrii <150890118+mernblockchainlover2019@users.noreply.github.com>
2024-03-19 05:49:20 +0200

    feat: add mobile responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,29 @@
 import styles from '@/styles/home.module.css';
 
 export default function Header() {
-    return <div className="mx-[72px] my-[48px] relative z-[1]">
+    return <div className={"my-[48px] relative z-[1] " + styles.header}>
         <div className="flex">
-            <img src="icon.svg" className="h-[50px]" />
+            <img src="icon.svg" className={styles.iconLogo} />
             <div className={"ml-2 mr-auto " + styles.textLogo}>Dexifier</div>
-            <div className="flex gap-8 mt-4">
+            <div className="show-on-desktop">
+                <div className="flex gap-8 mt-4">
+                    <a href="#" className={styles.textNavBar}>support</a>
+                    <a href="#" className={styles.textNavBar}>guides</a>
+                    <a href="#" className={styles.textNavBar}>docs</a>
+                    <a href="#" className={styles.textNavBar}>about us</a>
+                </div>
+            </div>
+        </div>
+        <span className={"ml-14 -mt-2 block " + styles.textLogoSubtitle}>home of alpha</span>
+        <div className="show-on-mobile">
+            <div className="flex mt-4">
+                <div className="flex gap-4 mx-auto">
                 <a href="#" className={styles.textNavBar}>support</a>
                 <a href="#" className={styles.textNavBar}>guides</a>
                 <a href="#" className={styles.textNavBar}>docs</a>
                 <a href="#" className={styles.textNavBar}>about us</a>
+                </div>
             </div>
         </div>
-        <span className={"ml-14 -mt-2 block " + styles.textLogoSubtitle}>home of alpha</span>
     </div>
 }
